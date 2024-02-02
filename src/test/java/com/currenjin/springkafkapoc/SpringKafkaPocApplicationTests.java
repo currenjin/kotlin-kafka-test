f7472cefad93700689be9f59a710d8c5a0187a4a
currenjin <hyun0524e@naver.com>
2024-02-02 09:02:37 +0900

    Add: init project

---
+++ src/test/java/com/currenjin/springkafkapoc/SpringKafkaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin.springkafkapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
